5b535080147328b0766dad1f57613583a592381e
Ray Johnson <changeforgrace@gmail.com>
2021-10-10 09:28:49 -0400

    first iteration ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { Container, Button, Header, Segment } from "semantic-ui-react";
+import logo from "./images/FullLogo-Footer.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => {
+	return (
+		<Container fluid>
+			<Segment basic inverted padded="very" vertical>
+				<Header as="h1" textAlign="center">
+					Power Stretch
+					<hr />
+				</Header>
+				<Header  textAlign="center">
+					One-on-one stretch and release work in New York City.
+				</Header>
+			</Segment>
+
+			<div className="value">
+				<h3>INCREASE RANGE OF MOTION</h3>
+				<h3>REMOVE RESTRICTIONS</h3>
+				<h3>REDUCE RISK OF INJURY</h3>
+				<h3>IMPROVE POSTURE</h3>
+				<h3>DECREASE WORKOUT RECOVERY TIME</h3>
+				<h3>LEARN PROPER SELF-MASSAGE ROUTINES</h3>
+
+				<a href="http://basharfitness.com/">
+					<Button size="huge space">Book Session</Button>
+				</a>
+			</div>
+
+			<div className="grid">
+				<h2 className="time-col">Available Every Day 8AM - 8PM</h2>
+				<div className="logo-col">
+					<img src={logo} alt="logo" />
+				</div>
+			</div>
+
+			<div className="footer">
+				<p>
+					<a href = "mailto:info@basharfitness.com">INFO@BASHARFITNESS.COM</a> | 917.545.3170 300-302 WEST 38TH ST, 3RD FLOOR
+				</p>
+			</div>
+		</Container>
+	);
+};
 
 export default App;
